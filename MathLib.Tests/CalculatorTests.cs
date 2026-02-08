c0ba3e6252a30140c0bd67fff5d26f693c59727e
dairexwolf <dairexwolf@gmail.com>
2026-02-08 20:49:33 +0700

    feat: calculator with 100% test coverage

---
+++ MathLib.Tests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using MathLib;
+
+namespace MathLib.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_TwoNums_ReturnsSum()
+        {
+
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Add(2, 3);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ MathLib/MathLib.cs
@@ -0,0 +1,49 @@
+﻿namespace MathLib
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// Adds to a b
+        /// </summary>
+        /// <param name="a">First num</param>
+        /// <param name="b">Second num</param>
+        /// <returns>a increased by b</returns>
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Decreases from a to b
+        /// </summary>
+        /// <param name="a">First num</param>
+        /// <param name="b">Second num</param>
+        /// <returns>a reduced by b</returns>
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Multiplies a and b
+        /// </summary>
+        /// <param name="a">First num</param>
+        /// <param name="b">Second num</param>
+        /// <returns>a multiplied by b</returns>
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Divides a by b
+        /// </summary>
+        /// <param name="a">First num</param>
+        /// <param name="b">Second num</param>
+        /// <returns>a divided by b</returns>
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
