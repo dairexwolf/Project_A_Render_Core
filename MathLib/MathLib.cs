7eab866fba7c337129e170cba7c0c810955cddff
dairexwolf <dairexwolf@gmail.com>
2026-02-17 22:42:08 +0700

    feat: implement Calculator.Divide overload method with exception handling

--- MathLib/MathLib.cs
+++ MathLib/MathLib.cs
@@ -44,6 +44,23 @@
         /// <exception cref="DivideByZeroException">When b is 0</exception>
         public int Divide(int a, int b)
         {
+            if (b == 0)
+                throw new DivideByZeroException("Division by zero value");
+            return a / b;
+        }
+
+        /// <summary>
+        /// Divides a by b with an epsilon precision 
+        /// </summary>
+        /// <param name="a">First num</param>
+        /// <param name="b">Second num</param>
+        /// <param name="epsilon">Precision</param>
+        /// <returns>a divided by b</returns>
+        /// <exception cref="DivideByZeroException">When b is near by 0</exception>
+        public float Divide(float a, float b, float epsilon=0.0001f)
+        {
+            if (Math.Abs(b) < epsilon)
+                throw new DivideByZeroException("Division by near-zero value");
             return a / b;
         }
     }
